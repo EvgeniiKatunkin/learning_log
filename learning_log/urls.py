4492ae79ba425d531253837716f37cc83aa2669c
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-05-20 10:40:33 +0500

    The base html page is created.

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('learning_logs.urls', namespace='learning_logs'))
+    path('', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
 ]
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,10 +1,11 @@
 """Defines URL patterns for learning_logs."""
 
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # Home page
-    url(r'^$', views.index, name='index')
+    path('', views.index, name='index')
 ]
